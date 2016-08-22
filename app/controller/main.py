47e987c71cf179686f990c9d66d3af6be0bc90d8
zyt4321 <zyt4321@qq.com>
2016-08-22 11:25:17 +0800

    redisControl add

--- app/controller/main.py
+++ app/controller/main.py
@@ -8,30 +8,33 @@ import logging
 # 微信消息接口
 @app.route('/',methods=["POST","GET"])
 def main():
-    signature = request.args.get("signature", "")
-    timestamp = request.args.get("timestamp", "")
-    nonce = request.args.get("nonce", "")
-    echostr = request.args.get("echostr", "")
+    try:
+        signature = request.args.get("signature", "")
+        timestamp = request.args.get("timestamp", "")
+        nonce = request.args.get("nonce", "")
+        echostr = request.args.get("echostr", "")
 
-    # 实例化Wechat类，传入设定在微信后台的token
-    wc = BaseWechat(config.token)
+        # 实例化Wechat类，传入设定在微信后台的token
+        wc = BaseWechat(config.token)
 
-    # echostr是微信用来验证服务器的参数，需原样返回
-    if echostr:
-        if wc.EchoTest(signature,timestamp,nonce,echostr):
-            return echostr
-        else:
-            logging.warning("TokenError")
-            return 'TokenFail'
+        # echostr是微信用来验证服务器的参数，需原样返回
+        if echostr:
+            if wc.EchoTest(signature,timestamp,nonce,echostr):
+                return echostr
+            else:
+                logging.warning("TokenError")
+                return 'TokenFail'
 
-    # 解析XML数据
-    wc.ParseXML(str(request.data))
+        # 解析XML数据
+        wc.ParseXML(str(request.data))
 
-    # 处理XML数据
-    wc.HandleData()
+        # 处理XML数据
+        wc.HandleData()
 
-    # 渲染返回值模板
-    return wc.RenderResult()
+        # 渲染返回值模板
+        return wc.RenderResult()
+    except Exception, e:
+        return e.message
 
 
 
